c8ea84333a2a81b739c2c9160ab0149b5fd77815
soheil1405 <msoheilamini@gmail.com>
2025-02-21 11:46:08 +0330

    token repo to create token

--- pkg/token/repository/token_repo.go
+++ pkg/token/repository/token_repo.go
@@ -7,24 +7,27 @@ import (
 	"gorm.io/gorm"
 )
 
-type UserRepository interface {
-	Create(token *models.Token) error
+type TokenRepository interface {
+	Create(token *models.Token) (*models.Token, error)
 	GetByID(ID database.PID) (*models.Token, error)
 }
 
-type userRepository struct {
+type tokenRepository struct {
 	db *gorm.DB
 }
 
-func NewUserRepository(db *gorm.DB) UserRepository {
-	return &userRepository{db}
+func NewTokenRepository(db *gorm.DB) TokenRepository {
+	return &tokenRepository{db}
 }
 
-func (r *userRepository) Create(token *models.Token) error {
-	return r.db.Create(token).Error
+func (r *tokenRepository) Create(token *models.Token) (*models.Token, error) {
+	if err := r.db.Create(token).Error; err != nil {
+		return nil, err
+	}
+	return token, nil
 }
 
-func (r *userRepository) GetByID(ID database.PID) (*models.Token, error) {
+func (r *tokenRepository) GetByID(ID database.PID) (*models.Token, error) {
 	var token models.Token
 	if err := r.db.First(&token, ID).Error; err != nil {
 		return nil, err
