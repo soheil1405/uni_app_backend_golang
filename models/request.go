3918c86ead4f256b46a37bb180928570abaafd2e
soheil1405 <msoheilamini@gmail.com>
2025-02-28 21:33:32 +0330

    add a helper for queries

--- models/request.go
+++ models/request.go
@@ -1,6 +1,11 @@
 package models
 
-import "uni_app/database"
+import (
+	"fmt"
+	"uni_app/database"
+
+	"gorm.io/gorm"
+)
 
 type FetchRequest struct {
 	ID       database.PID           `json:"id" query:"id"`
@@ -12,4 +17,39 @@ type FetchRequest struct {
 	Filters  map[string]interface{} `json:"filters" query:"filters"`
 	Includes []string               `json:"includes" query:"includes"`
 	Sorts    []string               `json:"sorts" query:"sorts"`
+}
+
+func (request *FetchRequest) PrepareQuery(query *gorm.DB) *gorm.DB {
+	var (
+		search  = request.Search
+		ids     = request.IDs
+		filters = request.Filters
+		sorts   = request.Sorts
+	)
+
+	for key, value := range filters {
+		switch v := value.(type) {
+		case string:
+			query = query.Where(fmt.Sprintf("%s = ?", key), v)
+		case []string:
+			query = query.Where(fmt.Sprintf("%s IN (?)", key), v)
+		default:
+			fmt.Printf("Unexpected type for key %s: %T\n", key, v)
+		}
+	}
+
+	if len(ids) > 0 {
+		query = query.Where("id in (?)", ids)
+	}
+
+	if search != "" {
+		searchStr := "%" + search + "%"
+		query = query.Where("name like ? ", searchStr)
+	}
+
+	for _, sort := range sorts {
+		query = query.Order(sort)
+	}
+
+	return query
 }
